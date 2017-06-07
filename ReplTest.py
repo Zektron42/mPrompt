714d6436f9df56aca2160220324275047508c735
Zektron42 <megadude42@gmail.com>
2017-06-06 21:37:40 -0700

    Connected it to an actual python prompt

--- ReplTest.py
+++ ReplTest.py
@@ -1,8 +1,9 @@
 from pygments.lexers import PythonLexer
 
 import pydoc
-
 import re
+import code
+import os
 
 from prompt_toolkit import prompt
 from prompt_toolkit.layout.lexers import PygmentsLexer
@@ -12,10 +13,24 @@ from prompt_toolkit.styles import style_from_dict
 from prompt_toolkit.token import Token
 from prompt_toolkit.buffer import Buffer
 
+os.system('cls')
+
 history = InMemoryHistory()
+mPrompt = code.InteractiveConsole()
+
+class ps():
+    def __init__(self):
+        cwd = os.getcwd()
+        cwd = '[' + cwd + ']\nPY> '
+
+    def __repr__(self):
+        cwd = os.getcwd()
+        cwd = '[' + cwd + ']\nPY> '
+        return cwd
+        
+ps1 = ps()
 
 def toolbar_tokens(cli):
-#    help(cli.current_buffer)
     result = cli.current_buffer.text
     matches = re.findall('[_a-zA-Z0-9\.]+\(', result[:cli.current_buffer.cursor_position])
     final = 'None'
@@ -32,17 +47,21 @@ def title():
     return 'MPrompt'
 
 style = style_from_dict({Token.Toolbar: '#ffffff bg:#333333',})
-
-myCompleter = WordCompleter(globals().keys())
+myCompleter = WordCompleter(list(mPrompt.locals.keys())+list(mPrompt.locals.get('__builtins__', {}).keys()))
 
 while True:
-    text = prompt('[Py] ',
+    text = prompt('PY> ',
                   lexer=PygmentsLexer(PythonLexer),
                   history=history,
                   get_bottom_toolbar_tokens=toolbar_tokens,
-                  completer=myCompleter,
-                  get_title=title)
+                  completer=myCompleter)
     if text.startswith('!'):
         print(f'[CMD] {text}')
     elif text.startswith('?'):
-        print(f'help({text})')
+        mPrompt.push(f'help({text[1:]})')
+    else:
+        mPrompt.push(text)
+        
+        
+    
+    myCompleter = WordCompleter(list(mPrompt.locals.keys())+list(mPrompt.locals.get('__builtins__', {}).keys()))
